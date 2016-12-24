1c6a87997dc74e5f9152f485bc52c8c749923c22
SANJANASRI-84284 <sanjusri962936@gmail.com>
2016-12-23 22:24:10 -0800

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int a;
+    printf("\nenter the number");
+    scanf("%d",&a);
+    if(a>0)
+        printf("\nit is positive");
+    else if(a<0)
+        printf("\nit is negative");
+    else
+        printf("\n it is zero");
+
+}
+
