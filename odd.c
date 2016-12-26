7b3a7824c50da5e3a5dafe0ea6118123ec6263d5
SANJANASRI-84284 <sanjusri962936@gmail.com>
2016-12-26 15:44:52 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a,b;
+printf("\nenter the starting limit :");
+scanf("%d",&a);
+printf("\nenter the ending limit:");
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+if(i%2==1)
+{
+printf("\n odd numbers are:%d",i);
+}}
+getch();}
